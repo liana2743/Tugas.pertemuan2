3a24af3c666753f3027f2ac535b2f977d407a755
liana2743 <lianaaprilia2704@gmail.com>
2022-09-29 08:09:47 +0700

    Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+        public function index()
+        {
+            echo "<h1>Perkenalkan</h1>";
+            echo "Nama saya Liana Nur Apriliani
+                  Saya tinggal di daerah Cibadak
+                  Olahraga yang saya sukai adalah Bulutangkis";
+        }   
+}
